64860c71847c4485dc48449006ef67a937a57309
Timothy Jones <t.jones@aptacode.com>
2019-12-03 17:23:04 +0000

    Added Senary and Septenary Nodes

---
+++ StateNet/NodeMachine/Choices/SenaryChoice.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aptacode.StateNet.NodeMachine.Choices
+{
+    public enum SenaryChoice
+    {
+        Item1,
+        Item2,
+        Item3,
+        Item4,
+        Item5,
+        Item6
+    }
+}
---
+++ StateNet/NodeMachine/Choices/SeptenaryChoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Aptacode.StateNet.NodeMachine.Choices
+{
+    public enum SeptenaryChoice
+    {
+        Item1,
+        Item2,
+        Item3,
+        Item4,
+        Item5,
+        Item6,
+        Item7
+    }
+}
