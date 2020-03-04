467292c13d20e0c30282eac8681951fcc2911005
Timothy Jones <t.jones@aptacode.com>
2020-03-04 14:47:32 +0000

    Added VisitCountWeightTests

---
+++ StateNetTests/ConnectionWeight/VisitCountWeightTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using Aptacode.StateNet.ConnectionWeight;
+using Aptacode.StateNet.Tests.Helpers;
+using NUnit.Framework;
+
+namespace Aptacode.StateNet.Tests.ConnectionWeight
+{
+    public class VisitCountWeightTests
+    {
+        public static IEnumerable<TestCaseData> ChangingHistoryTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(1, 1, 10, 20, 30, StateHistoryGenerator.History(), 10, "0 visits less than comparison return 10");
+                yield return new TestCaseData(1, 1, 10, 20, 30, StateHistoryGenerator.History(1), 20, "1 visits equals comparison return 20");
+                yield return new TestCaseData(1, 1, 10, 20, 30, StateHistoryGenerator.History(1,1), 30, "2 visits greater then comparison return 30");
+
+                yield return new TestCaseData(1, 1, 10, 20, 30, StateHistoryGenerator.History(2), 10, "0 visits less than comparison return 10");
+                yield return new TestCaseData(1, 2, 10, 20, 30, StateHistoryGenerator.History(2, 1), 10, "1 visits less than comparison return 10");
+                yield return new TestCaseData(1, 2, 10, 20, 30, StateHistoryGenerator.History(1, 2, 1), 20, "2 visits equals comparison return 20");
+                yield return new TestCaseData(1, 2, 10, 20, 30, StateHistoryGenerator.History(1, 2, 1, 2, 1), 30, "3 visits greater then comparison return 30");
+            }
+        }
+
+        [Test]
+        [TestCaseSource(nameof(ChangingHistoryTestCases))]
+        public void GetWeight_Returns_ExpectedWeight(int state, int comparisonCount, int lessThenWeight, int equalToWeight, int greaterThenWeight, List<State> history, int expectedWeight, string message = "")
+        {
+            Assert.AreEqual(expectedWeight, new VisitCountWeight(state.ToString(), comparisonCount, lessThenWeight, equalToWeight, greaterThenWeight).GetWeight(history), message);
+        }
+    }
+}
