eebd4e25219998f058d69f986e802a7ef6403d96
Timothy Jones <t.jones@aptacode.com>
2020-03-10 16:10:59 +0000

    Added ConnectionGroup Tests

---
+++ StateNetTests/ConnectionGroupTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace Aptacode.StateNet.Tests
+{
+    public class ConnectionGroupTests
+    {
+        [Test]
+        public void ConnectionGroup_Initialise_Empty()
+        {
+            var group = new ConnectionGroup();
+            Assert.AreEqual(0, group.GetAll().Count, "Connection Group Should have no StateDistribution's");
+            Assert.AreEqual(0, group.GetAllDistributions().ToList().Count, "Connection Group Should have no StateDistribution's");
+            Assert.AreEqual(0, group.GetAllActions().ToList().Count, "Connection Group Should have no Action's");
+        }
+
+        [Test]
+        public void CanCreateStateDistribution()
+        {
+            var group = new ConnectionGroup();
+            var distribution = group["Next"];
+            Assert.AreEqual(distribution, group["Next"]);
+        }
+        [Test]
+        public void DoesNotOverwriteStateDistribution()
+        {
+            var group = new ConnectionGroup();
+            var distribution1 = group["Next"];
+            var distribution2 = group["Next"];
+
+            Assert.AreEqual(distribution1, distribution2);
+        }
+
+        [Test]
+        public void CanGetAllActions()
+        {
+            var group = new ConnectionGroup();
+            var nextDistribution = group["Next"];
+            var backDistribution = group["Back"];
+            Assert.IsTrue(group.GetAllActions().SequenceEqual(new List<string> { "Next", "Back"}));
+        }
+
+        [Test]
+        public void CanGetAllConnections()
+        {
+            var group = new ConnectionGroup();
+            var nextDistribution = group["Next"];
+            var backDistribution = group["Back"];
+            Assert.AreEqual(2, group.GetAll().Count);
+        }
+    }
+}
