2a91df9a5f5527ed2ec01aef384a595252325233
Timothy Jones <t.jones@aptacode.com>
2020-03-25 18:41:31 +0000

    Added State Editor View

---
+++ StateNet.WPF/Views/StateEditor.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Aptacode.StateNet.WPF.Views
+{
+    /// <summary>
+    ///     Interaction logic for NetworkView.xaml
+    /// </summary>
+    public partial class StateEditor : UserControl
+    {
+        public StateEditor()
+        {
+            InitializeComponent();
+        }
+    }
+}
